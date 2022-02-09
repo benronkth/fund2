5fb6f670790b0c7c8f33a96c0c298f7fdf59c69c
Martin Larsson <martin.99.larsson@telia.com>
2022-02-09 15:44:44 +0100

    Add push endpoint

--- app/src/main/java/fund2/ContinuousIntegrationServer.java
+++ app/src/main/java/fund2/ContinuousIntegrationServer.java
@@ -4,10 +4,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.util.ajax.JSONObjectConvertor;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import fund2.tasks.Tasks;
 
 /**
  * Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -20,11 +27,31 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             HttpServletRequest request,
             HttpServletResponse response)
             throws IOException, ServletException {
+
+        String result = "";
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            result += line;
+        }
+        reader.close();
+        JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(result);
+        } catch (JSONException err) {
+            System.out.println(err.toString());
+            return;
+        }
+
+        if (target.equals("/push")) {
+            onPush(jsonObject);
+        }
+
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-
-        System.out.println(target);
 
         // here you do all the continuous integration tasks
         // for example
@@ -32,6 +59,17 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         // 2nd compile the code
 
         response.getWriter().println("CI job done");
+    }
+
+    public static void onPush(JSONObject json) {
+        System.out.println("Got push");
+        String ref = json.getString("ref");
+        String commit = json.getString("after");
+        String branch = ref.split("/")[2];
+        Tasks.gitTest(branch);
+        if (branch.equals("main")) {
+            Tasks.gitBuild(branch, commit);
+        }
     }
 
 }
