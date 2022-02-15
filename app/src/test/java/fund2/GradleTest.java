5f4c74e640b80637ae54368785d5900a93dd7853
Eyskha <chloe.porion@gmail.com>
2022-02-15 17:05:15 +0100

    test: positive test on Gradle build Test fails in Windows but to check in Linux
    Linked to #44

---
+++ app/src/test/java/fund2/GradleTest.java
@@ -0,0 +1,39 @@
+package fund2;
+
+import fund2.tasks.*;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.logging.Filter;
+
+import static org.junit.Assert.assertTrue;
+
+public class GradleTest {
+
+    // Positive tests
+    @Test
+    public void gradleBuildSuccessful () {
+        // Create new directory and go in
+        String workingDir = "./build/tmp/gradleBuildTest";
+        FileUtils.mkDirs(workingDir).execute();
+        FileUtils.setWorkingDir(workingDir).execute();
+
+        // Clone the project in this directory
+        Git.cloneRepo("https://github.com/fund-team/fund2.git").execute();
+
+        // Move one folder ahead
+        FileUtils.setWorkingDir(workingDir+"/fund2").execute();
+
+        // Build with gradle and take the result
+        TaskResult result = Gradle.build().execute();
+
+        // Return to app directory and remove the test directory
+        FileUtils.setWorkingDir(".").execute();
+        FileUtils.remove(workingDir).execute();
+
+        assertTrue(result.exitCode == 0);
+    }
+
+    // Negative tests
+}
